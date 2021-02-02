7ea2e43d30af555d1a2f730e78dcbb61446cebe4
Andre Silva <and.silva416@gmail.com>
2021-02-02 16:55:11 +0000

    updated

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+ const app = express()
+
+
+ app.get('/Home', (req, res) => {
+   res.sendFile( __dirname + '/views/Home.html')
+ })
+
+ 
+ app.get('/About', (req, res) => {
+    res.sendFile( __dirname + '/views/About.html')
+  
+ })
+
+ 
+ app.get('/Works', (req, res) => {
+    res.sendFile( __dirname + '/views/Works.html') 
+     
+ })
+
+ app.get('/PhotoGallery', (req, res) => {
+    res.sendFile( __dirname + '/views/PhotoGallery.html')
+     
+})
+
+app.listen(3000)
